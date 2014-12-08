99a4dd50da3e525ed59fc2477294d2642b419ee3
Matt Wujek <mattwujek@gmail.com>
2014-12-07 23:24:59 -0800

    sunday work
    need to fix color ranges/domain

--- js/wuj.js
+++ js/wuj.js
@@ -1,6 +1,88 @@
-/*jshint devel:true */
+  var $bars = $('.routeBar');
+var routeID;
 
-$(document).ready(function(){
+  function containerMargin (){
+      var height = $(window).height();
+      var $container = $('.container-lines');
+      $container.css('margin-top', height/4);
+  }
 
 
+function viewRoute(routeID){
+  // for hover effect
+  var $routeSVG; // SVG element in map
+  var $parentSVG = $('.svg-parent');  //selects parent container (used for 'z-index')
+
+  $routeSVG = $parentSVG.find(".bus-line-"+routeID);
+  $routeSVG.css('stroke','#2ecc71');
+  $parentSVG.append($routeSVG);
+  console.log($routeSVG);
+}
+
+//bus-line-
+
+ function removeRoute(routeID){
+//  // for hover effect
+
+var $routeSVG; // SVG element in map
+var $parentSVG = $('.svg-parent');  //selects parent container (used for 'z-index')
+
+  $routeSVG = $parentSVG.find(".bus-line-"+routeID);
+  $routeSVG.css('stroke','');
+
+  }
+
+
+  //$(document).ready(function(){
+
+  
+  //$allPaths =  $('.metro-path, .tep-path');
+
+
+  $(document).on({
+    mouseenter: function () {
+      var e = $(this);
+      routeID = $(this).data("route-id");
+      e.parent().addClass('hover-bar');
+      //console.log(e.parent());
+      //console.log(routeID);
+      //console.log('yo');
+      viewRoute(routeID);
+    },
+    mouseleave: function () {
+      var e = $(this);
+      e.parent().removeClass('hover-bar');
+      removeRoute(routeID);
+    }
+}, ".routeBar");
+
+  // $bars.hover(
+
+  //   function() {
+  //     var e = $(this);
+  //     routeID = $(this).data("route-id");
+  //     e.parent().addClass('hover-bar');
+  //     console.log(e.parent());
+  //     //console.log(routeID);
+  //     //console.log('yo');
+  //     viewRoute(routeID);
+  //     console.log('hy');
+
+  //   }, function() {
+  //     var e = $(this);
+  //     e.parent().removeClass('hover-bar');
+  //     removeRoute(routeID);
+  //   }
+  // );
+
+
+  //place container in the middle
+  
+
+  containerMargin(); // resize margins for container
+
+  $(window).resize(function() {
+      containerMargin();
 });
+
+//});
